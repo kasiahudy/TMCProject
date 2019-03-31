286bea59537720561bdd58cbee63e49fd17a1bc8
Patryk Iracki <patrak122@gmail.com>
2019-03-31 21:15:34 +0200

    Everything works but is ugly as something very ugly

--- src/main/java/pl/gdansk/skpt/MapCreatorREST/services/UserService.java
+++ src/main/java/pl/gdansk/skpt/MapCreatorREST/services/UserService.java
@@ -1,9 +1,14 @@
 package pl.gdansk.skpt.MapCreatorREST.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.gdansk.skpt.MapCreatorREST.model.SystemUser;
 import pl.gdansk.skpt.MapCreatorREST.security.MyUserDetails;
 
@@ -11,8 +16,20 @@ import javax.persistence.EntityManager;
 
 @Service
 public class UserService extends DBService<SystemUser> implements UserDetailsService {
+
+    @Autowired
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(11);
+    }
+
     public UserService(EntityManager entityManager) {
         super(entityManager, SystemUser.class, SystemUser::getLogin);
+    }
+
+    @Override
+    public void save(SystemUser entity) {
+        entity.setPassword(encoder().encode(entity.getPassword()));
+        super.save(entity);
     }
 
     @Override
